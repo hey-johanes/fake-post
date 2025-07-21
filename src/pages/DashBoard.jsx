4baec50d346ef659a0d57eba019b73a1a575be89
johnkristiadi <johnkristiadi@gmail.com>
2025-07-22 00:36:17 +0700

    update dashboard for add new item

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,52 @@
+import { Button } from 'react-bootstrap';
+import Footer from '../component/footer/Footer';
+import Header from '../component/navbar/Navbar';
+import { useEffect, useState } from 'react';
+import ModalsAddPost from '../component/modals/ModalsAddPost';
+import axios from 'axios';
+import CardItem from '../component/card/CardItem';
+
 function DashBoard() {
+  const [showModal, SetShowModal] = useState(false);
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:1234/post');
+        setDatas(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
   return (
     <div>
-      <p>ini halaman dashboard</p>
+      {showModal ? (
+        <ModalsAddPost show={showModal} setShow={SetShowModal} />
+      ) : (
+        <></>
+      )}
+      <Header />
+      <div style={{ marginTop: '80px', minHeight: '80vh' }}>
+        <div className="d-flex justify-content-center">
+          <Button variant="info" size="lg" onClick={() => SetShowModal(true)}>
+            Add Post
+          </Button>
+        </div>
+        <div className="m-5 container">
+          <div className="row">
+            {datas.map((data) => (
+              <CardItem
+                key={data.id}
+                username={data.username}
+                post={data.post}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
