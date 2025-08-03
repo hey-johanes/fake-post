d98ac0e03d6d9a68d4123dbf2b8764e0468e9273
johnkristiadi <johnkristiadi@gmail.com>
2025-08-04 00:21:54 +0700

    update navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const { logout, user } = useAuthContext();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
   return (
     <>
       <Navbar className="d-block" sticky="top" bg="dark" variant="dark">
-        <Container>
+        <Container className="d-flex">
           <Navbar.Brand className="text-light">Fake Post</Navbar.Brand>
-          <Nav className="me-auto text-light">
-            <Nav.Link href="/home" className="text-light">
-              Home
+          <Nav className="text-light">
+            <Nav.Link href="/dashboard" className="text-light">
+              Dashboard
             </Nav.Link>
-            <Nav.Link href="/features" className="text-light">
-              Features
+            <Nav.Link href="/social" className="text-light">
+              Social
             </Nav.Link>
             <Nav.Link href="/pricing" className="text-light">
               Pricing
             </Nav.Link>
           </Nav>
+          <div className="ms-auto d-flex">
+            <p className="text-light m-2">{`Hi, ${user.username}!`}</p>
+            <Button variant="outline-light" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
         </Container>
       </Navbar>
     </>
