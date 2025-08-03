6f44d023175c64c442405be625028c65df02aac0
johnkristiadi <johnkristiadi@gmail.com>
2025-08-04 00:22:49 +0700

    delete features.jsx

--- src/pages/Features.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Features() {
-  return (
-    <div style={{ marginTop: '80px', minHeight: '80vh' }}>
-      <h1>Features</h1>
-    </div>
-  );
-}
