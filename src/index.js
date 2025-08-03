93ed3da1b65353c0e60d312b20a41ff5cd3f199e
johnkristiadi <johnkristiadi@gmail.com>
2025-08-04 00:19:11 +0700

    add context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthContext';
+import { CardProvider } from './context/CardContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <CardProvider>
+        <App />
+      </CardProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
