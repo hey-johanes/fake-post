fab1e76b0182d623a4ec3dce6417a7ee8919d0e9
johnkristiadi <johnkristiadi@gmail.com>
2025-08-04 00:22:11 +0700

    create auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { useContext, createContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem('user');
+    try {
+      return savedUser ? JSON.parse(savedUser) : null;
+    } catch (err) {
+      console.warn('❗ user data in localStorage is not valid JSON:', err);
+      localStorage.removeItem('user');
+      return null;
+    }
+  });
+
+  const login = (userData) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  const value = {
+    user,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
