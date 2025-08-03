403031f27bec56363eed17810c22cf0b1e678141
johnkristiadi <johnkristiadi@gmail.com>
2025-08-04 00:22:30 +0700

    create card context

---
+++ src/context/CardContext.jsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { useContext, createContext, useState, useCallback } from 'react';
+import { useAuthContext } from './AuthContext';
+
+const CardContext = createContext();
+
+export function CardProvider({ children }) {
+  const [datas, setDatas] = useState([]);
+
+  const { user } = useAuthContext();
+
+  const fetchData = useCallback(async () => {
+    if (!user) return;
+    try {
+      const response = await axios.get(
+        `http://localhost:1234/post?userId=${user.id}`
+      );
+      setDatas(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [user]);
+
+  const fetchDataAll = useCallback(async () => {
+    if (!user) return;
+    try {
+      const response = await axios.get(`http://localhost:1234/post`);
+      setDatas(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [user]);
+
+  const deleteData = async (id) => {
+    try {
+      await axios.delete(`http://localhost:1234/post/${id}`);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const value = {
+    datas,
+    setDatas,
+    deleteData,
+    fetchData,
+    fetchDataAll,
+  };
+
+  return <CardContext.Provider value={value}>{children}</CardContext.Provider>;
+}
+
+export function useCardContext() {
+  return useContext(CardContext);
+}
